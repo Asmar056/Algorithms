00af2ba83beb62ec6bccd82b5ed27af9667af302
Asmar06 <asmar.ismayilova.06@outlook.com>
2021-08-19 11:29:12 +0400

    Initial commit

---
+++ src/CompareTriplets.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result1 {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        int A = 0; int B = 0;
+        for (int i = 0; i < 3; i++) {
+            if (a.get(i) > b.get(i)){
+                A++;
+            }else if(a.get(i) < b.get(i)){
+                B++;
+            }
+        }
+        return Arrays.asList(A,B);
+    }
+
+}
+
+public class CompareTriplets {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        String[] bTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bTemp[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = Result1.compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+            if (i != result.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        bufferedReader.close();
+    }
+}
---
+++ src/ef.java
@@ -0,0 +1,5 @@
+public class ef {
+    public static void main(String[] args) {
+        System.out.println(16.3%2);
+    }
+}
