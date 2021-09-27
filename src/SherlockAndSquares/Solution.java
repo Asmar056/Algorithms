d85edd702e91f8466f467f69d2c6f30dad7c68f2
Asmar06 <asmar.ismayilova.06@outlook.com>
2021-09-27 10:39:57 +0400

    'Sherlock and square' was added

---
+++ src/SherlockAndSquares/Solution.java
@@ -0,0 +1,56 @@
+package SherlockAndSquares;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b) {
+        // Write your code here
+        int c = 0, i = 1;
+        while (i*i < a) i++;
+        while (i*i <= b){
+            c++;
+            i++;
+        }
+        return c;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int a = Integer.parseInt(firstMultipleInput[0]);
+
+            int b = Integer.parseInt(firstMultipleInput[1]);
+
+            int result = Result.squares(a, b);
+
+            System.out.println(String.valueOf(result));
+        }
+
+        bufferedReader.close();
+    }
+}
+
